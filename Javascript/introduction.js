4f11c91712e53305cbcbbe66eafab5882a6cacdc
Manuela Moreno <mmorenoc2@eafit.edu.co>
2021-10-11 22:00:49 -0500

    Create introduction.js

---
+++ Javascript/introduction.js
@@ -0,0 +1,29 @@
+// Data types
+
+/** PRIMITIVE DATA TYPES 
+ * Numbers: Any number include decimals
+ *  Strings: Any group of characters
+ *  Booleans: true o false
+ *  Null:  intentional abscence of a value 'null'
+ *  Undefined: similar to null
+ *  
+ *  COMPLEX DATA TYPE
+ *  Symbol: unique identifiers
+ *  Object: collections of related data
+ */
+
+/** ARITHMETIC OPERATORS
+ * 
+ * Add: +
+ * Subtract: -
+ * Multiply: *
+ * Divide: /
+ * Remainder: %
+ * 
+ */
+
+var hola = 'Hola';
+
+hola = 'Hola 2';
+
+console.log(hola)
